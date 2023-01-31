02e8e8c4c52c4d07e6075f8ae7843f42e690525e
webson2020 <nrossettiossa@gmail.com>
2023-01-31 10:24:20 -0300

    update html and new profile.pdf

--- pdf.js
+++ pdf.js
@@ -1,9 +1,21 @@
 const puppeteer = require('puppeteer');
 
-(async() => {
+// (async() => {
+//     const browser = await puppeteer.launch();
+//     const page = await browser.newPage();
+//     await page.goto('https://webson2020.github.io/', {waitUntil:'networkidle2'});
+//     await page.pdf({path:'pdf.pdf', format: 'A4'});
+//     await browser.close();
+// })();
+
+
+
+async function convertToPDF() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://webson2020.github.io/', {waitUntil:'networkidle2'});
-    await page.pdf({path:'pdf.pdf', format: 'A4'});
+    await page.goto('https://webson2020.github.io/');
+    await page.pdf({path: 'profile.pdf', format: 'A4'});
     await browser.close();
-})();
+  }
+  
+  convertToPDF();
